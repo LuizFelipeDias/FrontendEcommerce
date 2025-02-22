0c92564114c57ef56f084119c04e43a6cfb2c807
unknown <lfdias2203@gmail.com>
2025-02-22 15:02:53 -0400

    testes QA

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -52,7 +52,14 @@ const Header = () => {
 
       <nav>
         <ul>
-          <li><Link to="/">Home</Link></li>
+          <li>
+            <Link 
+              to="/" 
+              data-testid={location.pathname === "/" ? "active-category-link" : "category-link"}
+            >
+              Home
+            </Link>
+          </li>
           <li>
             <Link 
               to="/all" 
