942ae4008a90db47eca383fe63b9b998f942c2af
unknown <lfdias2203@gmail.com>
2025-02-22 13:05:35 -0400

    testes QA

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartShopping, faPlus, faMinus, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faSearch, faCartShopping, faPlus, faMinus, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
-import { useCart} from "../Cart/CartContext";  // caminho corrigido
+import { useCart } from "../Cart/CartContext";
 import "./Header.css";
 import Logo from "../../assets/Logo";
 
