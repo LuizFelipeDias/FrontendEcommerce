fb59c031237eac11b53f362791edba13e6bcb6ce
unknown <lfdias2203@gmail.com>
2025-02-23 15:32:03 -0400

    testes

--- src/Components/Cart/CartContext.jsx
+++ src/Components/Cart/CartContext.jsx
@@ -4,25 +4,26 @@ const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
-  const [isCartOverlayOpen, setIsCartOverlayOpen] = useState(false);
 
   const addToCart = (product) => {
     setCartItems((prevItems) => {
+      // Filtra os produtos que têm os mesmos atributos do novo produto
       const matchingItems = prevItems.filter(
         (item) => JSON.stringify(item.attributes) === JSON.stringify(product.attributes)
       );
 
       if (matchingItems.length > 0) {
+        // Se existirem produtos com os mesmos atributos, aumenta a quantidade de cada um
         return prevItems.map((item) =>
           JSON.stringify(item.attributes) === JSON.stringify(product.attributes)
             ? { ...item, quantity: item.quantity + 1 }
             : item
         );
       } else {
+        // Se não existir, adiciona um novo produto ao carrinho
         return [...prevItems, { ...product, quantity: 1, uniqueId: Date.now() }];
       }
     });
-    setIsCartOverlayOpen(true); // Abre o overlay ao adicionar um item
   };
 
   const updateCartItemQuantity = (uniqueId, newQuantity) => {
@@ -49,27 +50,8 @@ export const CartProvider = ({ children }) => {
     setCartItems((prevItems) => prevItems.filter((item) => item.uniqueId !== uniqueId));
   };
 
-  const openCartOverlay = () => {
-    setIsCartOverlayOpen(true); // Função para abrir o overlay
-  };
-
-  const closeCartOverlay = () => {
-    setIsCartOverlayOpen(false); // Função para fechar o overlay
-  };
-
   return (
-    <CartContext.Provider
-      value={{
-        cartItems,
-        addToCart,
-        updateCartItemQuantity,
-        updateCartItemAttributes,
-        removeFromCart,
-        isCartOverlayOpen,
-        openCartOverlay,
-        closeCartOverlay,
-      }}
-    >
+    <CartContext.Provider value={{ cartItems, addToCart, updateCartItemQuantity, updateCartItemAttributes, removeFromCart }}>
       {children}
     </CartContext.Provider>
   );
