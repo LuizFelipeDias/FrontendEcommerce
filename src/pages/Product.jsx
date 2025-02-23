d00792d6e736773246d1d1e4242b27446b96b421
unknown <lfdias2203@gmail.com>
2025-02-23 09:45:10 -0400

    testes QA

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,124 +1,154 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleInfo, faCartShopping } from "@fortawesome/free-solid-svg-icons";
-import { useCart } from "../Components/Cart/CartContext";
+import { faSearch, faCartShopping, faPlus, faMinus, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { Link, useLocation } from "react-router-dom";
+import { useCart } from "../Cart/CartContext";
+import "./Header.css";
+import Logo from "../../assets/Logo";
 
-
-
-const Product = () => {
-  const [products, setProducts] = useState([]);
-  const navigate = useNavigate();
-  const { addToCart } = useCart();
+const Header = () => {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const { cartItems = [], updateCartItemQuantity, updateCartItemAttributes, removeFromCart } = useCart();
+  const location = useLocation();
 
   useEffect(() => {
-    fetch("https://backend-production-6806.up.railway.app/backend/consultas/product/productImage.php")
-      .then((response) => response.json())
-      .then((data) => {
-        if (Array.isArray(data)) {
-          setProducts(data);
-        } else {
-          setProducts([]);
-        }
-      })
-      .catch(() => setProducts([]));
-  }, []);
+    console.log("Pathname atualizado:", location.pathname);
+  }, [location.pathname]);
 
-  const handleProductClick = (product) => {
-    navigate(`/product/${product.id}`, { state: { product } });
+  const handleCartClick = () => {
+    setIsCartOpen((prevState) => !prevState);
   };
 
-  const handleAddToCart = (product) => {
-    if (!product || product.in_stock === 0) return;
-
-    const selectedAttributes = product.attributes?.reduce((acc, attr) => {
-      if (!acc[attr.name]) {
-        acc[attr.name] = attr.value;
-      }
-      return acc;
-    }, {});
-
-    const uniqueId = `${product.id}-${Object.entries(selectedAttributes || {})
-      .sort((a, b) => a[0].localeCompare(b[0]))
-      .map(([key, value]) => `${key}:${value}`)
-      .join("-")}`;
-    
-    // Cria um objeto com os atributos disponíveis
-    const availableAttributes = product.attributes?.reduce((acc, attr) => {
-      acc[attr.name] = acc[attr.name] || [];
-      acc[attr.name].push(attr.value);
-      return acc;
-    }, {});
-
-    const cartItem = {
-      id: product.id,
-      uniqueId,
-      name: product.name,
-      image: product.images?.[0] || "https://via.placeholder.com/300",
-      price: parseFloat(product.amount || 0).toFixed(2),
-      currency: product.currency_symbol,
-      attributes: { ...selectedAttributes }, // Copia os atributos selecionados
-      availableAttributes, // Inclui os atributos disponíveis
-      quantity: 1,
-    };
-
-    addToCart(cartItem);
-    console.log("Produto adicionado ao carrinho:", cartItem);
+  const handleQuantityChange = (uniqueId, newQuantity) => {
+    if (newQuantity < 1) {
+      removeFromCart(uniqueId);
+    } else {
+      updateCartItemQuantity(uniqueId, newQuantity);
+    }
   };
 
+  const handleAttributeChange = (uniqueId, attributeName, newValue) => {
+    const item = cartItems.find((item) => item.uniqueId === uniqueId);
+    if (item) {
+      const updatedAttributes = {
+        ...item.attributes,
+        [attributeName]: newValue,
+      };
+      updateCartItemAttributes(uniqueId, updatedAttributes);
+    }
+  };
+
+  const calculateTotal = () => {
+    return cartItems.reduce((total, item) => total + item.price * item.quantity, 0).toFixed(2);
+  };
+
+  const toKebabCase = (str) => str.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase().replace(/\s+/g, "-");
 
   return (
-    <div>
-      <div className="page-inner-content">
-        <div className="product-list grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-          {products.length > 0 ? (
-            products.map((product) => (
-              <div
-                key={product.id}
-                className="product border rounded-lg p-4 shadow-md cursor-pointer relative product-card"
-                onClick={() => handleProductClick(product)}
+    <div className="header">
+      <div className={`overlay ${isCartOpen ? "active" : ""}`} onClick={handleCartClick}></div>
+
+      <div className="logo-container">
+        <Logo />
+      </div>
+
+      <nav>
+        <ul>
+          {["Home", "all", "clothes", "tech"].map((category) => (
+            <li key={category}>
+              <Link
+                to={`/${category === "Home" ? "" : category}`}
+                data-testid={location.pathname === `/${category === "Home" ? "" : category}` ? "active-category-link" : "category-link"}
               >
-                <div className={`image ${product.in_stock === 0 ? "out-of-stock" : ""}`}>
-                  {product.images?.length > 0 ? (
-                    <img src={product.images[0]} alt={product.name} className="w-full h-40 object-cover rounded" />
-                  ) : (
-                    <p className="text-center text-gray-500">Sem imagem disponível</p>
-                  )}
-                  {product.in_stock === 0 && <div className="out-of-stock-label">Out of Stock</div>}
+                {category}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      <div className="cart-container">
+        <button onClick={handleCartClick} className="cart-btn" data-testid="cart-btn">
+          <FontAwesomeIcon icon={faCartShopping} className="cart" />
+          <div className="products-count">{cartItems.length}</div>
+        </button>
+
+        <div className={`cart-modal ${isCartOpen ? "active" : ""}`} data-testid="cart-overlay" style={{ pointerEvents: isCartOpen ? "auto" : "none" }}>
+          <button className="close-modal" onClick={handleCartClick}>
+            <FontAwesomeIcon icon={faTimes} />
+          </button>
+          <div className="cart-modal-content">
+            <h2 className="cart-title">YOUR BAG</h2>
+            <div className="cart-items-container">
+              {cartItems.map((item, index) => (
+                <div key={index} className="cart-item-container">
+                  <div className="cart-item-details">
+                    <p className="cart-item-name">{item.name}</p>
+                    <p className="cart-item-price">{item.price} {item.currency}</p>
+
+                    {item.availableAttributes && (
+                      <div className="cart-item-attributes">
+                        {Object.entries(item.availableAttributes).map(([groupName, attributes]) => {
+                          const kebabCaseGroupName = toKebabCase(groupName);
+                          return (
+                            <div key={groupName} className="cart-attribute-group">
+                              <h4 className="cart-attribute-title">{groupName}:</h4>
+                              <div className="cart-attribute-buttons">
+                                {attributes.map((option) => {
+                                  const kebabCaseOption = toKebabCase(option);
+                                  const isSelected = item.attributes[groupName] === option;
+                                  return (
+                                    <button
+                                      key={option}
+                                      className={`cart-attribute-button ${isSelected ? "selected" : ""}`}
+                                      style={groupName.toLowerCase() === "color" ? { backgroundColor: option } : {}}
+                                      onClick={() => handleAttributeChange(item.uniqueId, groupName, option)}
+                                      data-testid={`cart-item-attribute-${kebabCaseGroupName}-${kebabCaseOption}${isSelected ? "-selected" : ""}`}
+                                    >
+                                      {groupName.toLowerCase() !== "color" && option}
+                                    </button>
+                                  );
+                                })}
+                              </div>
+                            </div>
+                          );
+                        })}
+                      </div>
+                    )}
+                  </div>
+
+                  <div className="add-and-remove-cart">
+                    <div className="cart-item-quantity">
+                      <button className="add-quantity" onClick={() => handleQuantityChange(item.uniqueId, item.quantity + 1)}
+                        data-testid="cart-item-amount-increase">
+                        <FontAwesomeIcon icon={faPlus} className="add"/>
+                      </button>
+                      <input
+                        className="input-quntity"
+                        type="number"
+                        value={item.quantity}
+                        min="1"
+                        onChange={(e) => handleQuantityChange(item.uniqueId, parseInt(e.target.value))}
+                      />
+                      <button className="remove-quantity" onClick={() => handleQuantityChange(item.uniqueId, item.quantity - 1)}
+                        data-testid="cart-item-amount-decrease">
+                        <FontAwesomeIcon icon={faMinus}/>
+                      </button>
+                    </div>
+                  </div>
+
+                  <div className="cart-item-image-container">
+                    <img src={item.image} alt={item.name} className="cart-item-image" />
+                  </div>
                 </div>
-
-                <h2 className="product-text">{product.name}</h2>
-
-                <p className="price-text">
-                  {parseFloat(product.amount).toFixed(2)} {product.currency_symbol}
-                </p>
-
-                <div className="info-and-cart">
-                  <button className="product-info">
-                    See Details <FontAwesomeIcon icon={faCircleInfo} />
-                  </button>
-                </div>
-
-                {product.in_stock > 0 && (
-                  <button
-                    className="quick-buy-btn"
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      handleAddToCart(product);
-                    }}
-                  >
-                    <FontAwesomeIcon icon={faCartShopping} />
-                  </button>
-                )}
-              </div>
-            ))
-          ) : (
-            <p>Carregando produtos...</p>
-          )}
+              ))}
+            </div>
+            <p className="total">TOTAL: {calculateTotal()}</p>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default Product;
+export default Header;
